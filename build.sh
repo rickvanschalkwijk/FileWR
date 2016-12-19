a59c6e9156c6f78285a2bc29538f555a533f3406
Rick van Schalkwijk <rick.van.schalkwijk@kvk.nl>
2016-12-19 15:30:44 +0100

    changes slashes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 dotnet restore && dotnet build **/project.json
-dotnet test .\Source\FileWR.Tests\
-dotnet test .\Source\FileWR.Business.Tests\
+dotnet test ./Source/FileWR.Tests/
+dotnet test ./Source/FileWR.Business.Tests/
